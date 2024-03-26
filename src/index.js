d43e6425024e6c0cb2bfe9c95141c44939035129
sonletrung <146707549+sonletrung@users.noreply.github.com>
2024-03-26 11:39:26 +0700

    keo dev

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
+
                 <App/>
-            </DevSupport>
+
         </BrowserRouter>
     </Provider>
 );
