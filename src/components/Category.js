af4943c3a1e8ce855366618fdcc313c7a1ed6d4c
sonletrung <146707549+sonletrung@users.noreply.github.com>
2024-03-27 15:34:09 +0700

    chuyen nhanh

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getCategories} from "../services/category/categoryService";
 import {Link} from "react-router-dom";
-
+import "./Category.css"
 function Category() {
     const propertyItems = [
         {icon: 'img/icon-apartment.png', title: 'Apartment', properties: '123 Properties', delay: '0.1s'},
@@ -14,15 +14,23 @@ function Category() {
         {icon: 'img/icon-condominium.png', title: 'Shop', properties: '123 Properties', delay: '0.5s'},
         {icon: 'img/icon-luxury.png', title: 'Garage', properties: '123 Properties', delay: '0.7s'}
     ];
+
     let dispatch = useDispatch()
     let categories = useSelector(state => {
         return state.categories.categories
     })
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
+    const [indexOfLastItem, setIndexOfLastItem] = useState(0);
+
     useEffect(() => {
         dispatch(getCategories())
     }, [])
     const renderPropertyItems = () => {
-        return categories.map((category, index) => (
+        const indexOfLastItem = currentPage * itemsPerPage; // Tính chỉ mục cuối cùng của mục cần hiển thị.
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage; // Tính chỉ mục đầu tiên của mục cần hiển thị.
+        const currentItems = categories.slice(indexOfFirstItem, indexOfLastItem);
+        return currentItems.map((category, index) => (
             <div key={index} className={`col-lg-3 col-sm-6 wow fadeInUp`} data-wow-delay={`0.5s`}>
                 <Link to={``} className="cat-item d-block bg-light text-center rounded p-3">
                     <div className="rounded p-4">
@@ -36,6 +44,19 @@ function Category() {
             </div>
         ));
     };
+    const handleNextPage = () => {
+        const nextIndexOfLastItem = currentPage * itemsPerPage;
+        setIndexOfLastItem(nextIndexOfLastItem);
+        setCurrentPage(currentPage + 1);
+    };
+
+
+    const handlePrevPage = () => {
+        setCurrentPage(currentPage - 1);
+    };
+    const totalItems = categories.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const isLastPage = currentPage === totalPages;
     return (
         <>
             <div className="container-xxl py-5">
@@ -46,8 +67,21 @@ function Category() {
                         <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum
                             sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
                     </div>
-                    <div className="row g-4">
-                        {renderPropertyItems()}
+                    <div className="wrapper ">
+                        <button className="icon-container" onClick={handlePrevPage} disabled={currentPage === 1}>
+                            <i className="bi bi-chevron-left"></i>
+                        </button>
+                        <div className="content-wrapper">
+                            <div className="inner-wrapper">
+                                <div className="row g-4">
+                                    {renderPropertyItems()}
+                                </div>
+                            </div>
+                        </div>
+                        <button className="icon-container" onClick={handleNextPage} disabled={isLastPage || ((totalItems % itemsPerPage === 0) && currentPage === totalPages)}
+                        >
+                            <i className="bi bi-chevron-right"></i>
+                        </button>
                     </div>
                 </div>
             </div>
